1a3d5f2c1c4f62620da215bb1412c57f3f1f1af2
MaxV16 <maks.karakulinn@gmail.com>
2025-10-02 22:02:56 +0100

    Mark task as done

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -9,20 +9,28 @@ function App() {
     tasks: [
       { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", priority: "high" },
      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", priority: "medium" },
-     { id: 3, title: "Tidy up", deadline: "Today", priority: "low" }
+     { id: 3, title: "Tidy up", deadline: "Today", priority: "low", done: false }
     ]
   });
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({tasks});
+  }
+
 
   return (
     <div className="container">
       <h1>Tasky</h1>
-    {taskState.tasks.map((task) => (              
-    <Task 
+    {taskState.tasks.map((task, index) => (
+    <Task
       title={task.title}
       description={task.description}
       deadline={task.deadline}
       priority={task.priority}
       key={task.id}
+      done={task.done}
+      markDone={() => doneHandler(index)}
     />
   ))}
     </div>
--- tasky/src/components/Tasks.jsx
+++ tasky/src/components/Tasks.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const Tasks = (props) => {
     
     return (
-        <div className="card"> 
+        <div className="card" style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}>
         <p className="title">{props.title}</p>
        <p><b>{props.priority}</b></p>
        <p> Due: {props.deadline}</p>
-       {props.description && <p className="task-description">{props.description}</p>}
+       {props.description !== "" && <p className="task-description">{props.description}</p>}
+       <button onClick={props.markDone} className='doneButton'>Done</button>
+
        </div>
        
     )
